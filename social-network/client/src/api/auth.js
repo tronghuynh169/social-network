f85bcb9775c340f28e53fe787ee464dcafc0583d
tronghuynh1609 <tronghuynh16092004123@gmail.com>
2025-03-17 20:38:17 +0700

    [update] path

--- social-network/client/src/api/auth.js
+++ social-network/client/src/api/auth.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/auth';
+const API_URL = 'http://localhost:5000/api';
 
 export const register = async (userData) => {
-    return axios.post(`${API_URL}/register`, userData);
+    return axios.post(`${API_URL}/auth/register`, userData);
 };
 
 export const login = async (userData) => {
-    return axios.post(`${API_URL}/login`, userData);
+    return axios.post(`${API_URL}/auth/login`, userData);
 };
--- social-network/server/routes/index.js
+++ social-network/server/routes/index.js
@@ -4,7 +4,7 @@ function route(app) {
     app.get('/', (req, res) => {
         res.send('Welcome to Social Network API!');
     });
-    app.use('/auth', authRouter);
+    app.use('/api/auth', authRouter);
 }
 
 module.exports = route;
